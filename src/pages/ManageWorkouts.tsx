53cf1708d06bfbe8a950f1087c402af3b514b3c0
Julian Ng <julesnuggy@gmail.com>
2023-10-08 13:00:26 +0100

    Implement Button in ManageWorkouts

--- src/pages/ManageWorkouts.tsx
+++ src/pages/ManageWorkouts.tsx
@@ -1,14 +1,15 @@
+import Button from '../components/Button';
 import styles from './ManageWorkouts.module.scss';
 
 const ManageWorkouts = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Manage Workouts</h2>
-      <button className={styles.start}>Start New Workout</button>
+      <Button className={styles.start}>Start New Workout</Button>
       <p className={styles.info_text}>Click/Tap on an exercise below to view/edit it.</p>
-      <button className={styles.history}>Workout 3</button>
-      <button className={styles.history}>Workout 2</button>
-      <button className={styles.history}>Workout 1</button>
+      <Button className={styles.history}>Workout 3</Button>
+      <Button className={styles.history}>Workout 2</Button>
+      <Button className={styles.history}>Workout 1</Button>
     </div>
   );
 };
