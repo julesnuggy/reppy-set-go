abc0afd4e950de4a51ad8cd9418d436b69b09bb0
Julian Ng <julesnuggy@gmail.com>
2023-10-08 09:22:11 +0100

    Create Header bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import barbell from './assets/icons/barbell-red.svg';
+import weightlifter from './assets/icons/weightlifter.svg';
 import styles from './App.module.scss'
 
 const App = () => {
   return (
     <div className={styles.app_container}>
-      <header className={styles.header}></header>
+      <header className={styles.header}>
+        <img className={styles.icon} src={barbell} alt='barbell-menu' />
+        <h1>Reppy, Set, Go!</h1>
+        <img className={styles.icon} src={weightlifter} alt='weightlifter-user-account' />
+      </header>
       <main className={styles.main}></main>
     </div>
   )
