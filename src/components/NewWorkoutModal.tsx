5996f87803b1974747021433e9d9d28174967ec5
Julian Ng <julesnuggy@gmail.com>
2023-10-15 11:49:22 +0100

    Run linter

--- src/components/NewWorkoutModal.tsx
+++ src/components/NewWorkoutModal.tsx
@@ -4,19 +4,14 @@ import styles from './NewWorkoutModal.module.scss';
 
 type ModalProps = {
   isShown: boolean;
-}
+};
 
 const DAYS_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const BackDrop = (props) => (
-  <div
-    {...props}
-    className={styles.backdrop}
-  />
-)
+const BackDrop = (props) => <div {...props} className={styles.backdrop} />;
 
 const NewWorkoutModal = ({ isShown }: ModalProps) => {
-  const todayString = new Date().toISOString().substring(0,10);
+  const todayString = new Date().toISOString().substring(0, 10);
   const [workoutDate, setWorkoutDate] = useState(todayString);
 
   const handleClick = () => {
@@ -26,7 +21,7 @@ const NewWorkoutModal = ({ isShown }: ModalProps) => {
   const getWorkoutDay = () => {
     const dayIndex = new Date(workoutDate).getDay();
     return DAYS_OF_WEEK[dayIndex].slice(0, 3);
-  }
+  };
 
   return (
     <Modal show={isShown} renderBackdrop={BackDrop}>
@@ -34,21 +29,19 @@ const NewWorkoutModal = ({ isShown }: ModalProps) => {
         <div className={styles.config}>
           <div className={styles.config__name}>
             <label htmlFor="workout-name">Name:</label>
-            <input id="workout-name" autoFocus={true} placeholder="Give your workout a name"></input>
+            <input id="workout-name" placeholder="Give your workout a name" />
           </div>
 
           <div className={styles.config__date}>
-            <label htmlFor="workout-date">
-              Date:
-            </label>
+            <label htmlFor="workout-date">Date:</label>
             <span>{getWorkoutDay()}</span>
-            <input id="workout-date" type="date" value={workoutDate} onChange={e => setWorkoutDate(e.target.value)}></input>
+            <input id="workout-date" type="date" value={workoutDate} onChange={(e) => setWorkoutDate(e.target.value)} />
             <button onClick={handleClick}>Today</button>
           </div>
         </div>
       </div>
     </Modal>
-  )
-}
+  );
+};
 
 export default NewWorkoutModal;
